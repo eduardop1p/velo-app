a6b3def62671c5883660e603d47d4f5fa0570a37
eduardop1p <el702985@gmail.com>
2024-01-28 22:02:01 -0300

    possivel correção na index da api

--- src/app/api/route.ts
+++ src/app/api/route.ts
@@ -4,16 +4,13 @@ export const dynamic = 'force-dynamic'; // defaults to auto
 
 // eslint-disable-next-line
 export async function GET(request: Request) {
-  return NextResponse.json(
-    { status: 200 },
-    {
-      status: 200,
-      headers: {
-        // subtstituir * pela origin certa do domain
-        'Access-Control-Allow-Origin': '*',
-        'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, OPTIONS',
-        'Access-Control-Allow-Headers': 'Content-Type, Authorization',
-      },
-    }
-  );
+  return new Response(JSON.stringify({ status: 'OK' }), {
+    status: 200,
+    headers: {
+      // subtstituir * pela origin certa do domain
+      'Access-Control-Allow-Origin': '*',
+      'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, OPTIONS',
+      'Access-Control-Allow-Headers': 'Content-Type, Authorization',
+    },
+  });
 }
