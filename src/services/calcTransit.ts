a96c4a0d72b508861b7562daaba72f19dd4dcb6b
eduardop1p <el702985@gmail.com>
2024-02-23 10:04:56 -0300

    bugs #3

--- src/services/calcTransit.ts
+++ src/services/calcTransit.ts
@@ -3,7 +3,7 @@ import { TransactionsType } from '@/app/api/models/users';
 export default function calTransit(transactions: TransactionsType[]) {
   const totalTransit = transactions.reduce((prev, transactions) => {
     if (transactions.status === 'pending') {
-      return prev + transactions.dollarValue;
+      return prev + (transactions.dollarValue || 0);
     }
     return prev;
   }, 0);
